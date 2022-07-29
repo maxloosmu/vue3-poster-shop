ebf2faf11ce8ca97151d232fb44e5a619d7a99c0
Max Loo <maxloo@smu.edu.sg>
2022-07-29 12:48:29 +0800

    changed .circleci/config.yml, server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV !== "production") {
 	require("reload")(app);
 }
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 1000;
 server.listen(port, function () {
 	console.log("Listening on port ".concat(port));
 });
