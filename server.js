73336fa7e7f484778bae7b6c9f490e5b887d458f
Max Loo <maxloo@smu.edu.sg>
2022-08-17 14:40:03 +0800

    added server2.js, changed server.js, package.json

--- server.js
+++ server.js
@@ -21,28 +21,28 @@ app.get("/", function(req, res) {
 
 var directory;
 fs.readFile("./directory.json", "utf8", function(err, data) {
-	console.log(data.substring(0, 50));
-	directory = JSON.parse(data);
-	console.log(directory[0]);
-	if (err) {
-		throw err;
-	}
+  console.log(data.substring(0, 50));
+  directory = JSON.parse(data);
+  console.log(directory[0]);
+  if (err) {
+    throw err;
+  }
 });
 
 app.get("/search", function(req, res) {
-	console.log(req.query);
-	var results = directory.reduce(function(acc, file) {
-		if (file.tags.indexOf(req.query.q) !== -1) {
-			acc.push({
-				id: file.id,
-				title: file.title,
-			  thumb: "/public/images/".concat(file.thumb),
-		    price: file.price
-		  });
-	  }
-	  return acc;
- 	}, []);
-	res.send(results);
+  console.log(req.query);
+  var results = directory.reduce(function(acc, file) {
+    if (file.tags.indexOf(req.query.q) !== -1) {
+      acc.push({
+        id: file.id,
+        title: file.title,
+        thumb: "/public/images/".concat(file.thumb),
+        price: file.price
+      });
+    }
+    return acc;
+  }, []);
+  res.send(results);
 });
 
 app.get('/headers', (req, res) => {
@@ -81,24 +81,24 @@ app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 app.use("/public", express.static(path.join(__dirname, "public")));
 
 if (process.env.NODE_ENV !== "production") {
-	require("reload")(app);
+  require("reload")(app);
 }
 
 var port = process.env.PORT || 8050;
 var port2 = process.env.PORT || 8060;
 server.listen(port, function () {
-	console.log("Listening on port ".concat(port));
+  console.log("Listening on port ".concat(port));
 });
 if (!fs.existsSync(privkey)) {
-	console.log("privkey not found");
+  console.log("privkey not found");
 }
 else {
   var options = {
-	  key: fs.readFileSync(privkey),
-	  cert: fs.readFileSync(seccert)
+    key: fs.readFileSync(privkey),
+    cert: fs.readFileSync(seccert)
   };
   server2 = https.createServer(options, app);
   server2.listen(port2, function () {
-		console.log("Listening on port ".concat(port2));
-	});
+    console.log("Listening on port ".concat(port2));
+  });
 }
